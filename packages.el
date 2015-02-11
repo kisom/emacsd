083b8b8391f7b72485f69154ca7a73aa7f63acb2
Kyle Isom <kyle@cloudflare.com>
2015-02-11 01:07:46 -0800

    Update initial package lists.

--- packages.el
+++ packages.el
@@ -1,52 +1,32 @@
 ;;; This is a list of packages that are currently installed. This file
 ;;; should be compiled when the emacs lisp files are first loaded.
 
-(require 'cl)
 (require 'package)
 (package-initialize)
 
-(defun andl (lst)
-  "Apply the #'and function to a list of truth values."
-  (reduce (lambda (a b)
-	    (and a b))
-	  lst))
-
-(defun orl (lst)
-  "Apply the #'or function to a list of truth values."
-  (reduce (lambda (a b)
-	    (or a b))
-	  lst))
-
-(defun marmalade-set-p ()
-  (orl
-   (mapcar (lambda (repo)
-	     (string-equal (car repo) "marmalade"))
-	   package-archives)))
-
 (setq initial-package-list
-      '("flymake-haskell-multi"
-	"flymake-hlint"
-	"ghc"
-	"ghci-completion"
-	"haskell-mode"
-	"hippie-expand-haskell"
-	"sublime-themes"
-	"tronesque"
-	"magit"
-	"paredit"
-	"slime"
-	"undo-tree"
-	"rainbow-delimiters"))
+      '(
+	;flymake-haskell-multi
+	;flymake-hlint
+	;ghc
+	;ghci-completion
+	;haskell-mode
+	;hippie-expand-haskell
+	;sublime-themes
+	;tronesque-theme
+	magit
+	paredit
+	slime
+	undo-tree
+	rainbow-delimiters))
 
-(defun bootstrap-packages ()
-  (unless (marmalade-set-p)
-    (add-to-list 'package-archives
-		 '("marmalade" . "http://marmalade-repo.org/packages/")))
-  (package-refresh-contents)
-  (mapcar #'package-install 
-	  (mapcar #'make-symbol initial-package-list)))
-
-(bootstrap-packages)
+(add-to-list 'package-archives
+   '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+   '("melpa" . "http://melpa.org/packages/"))
+(package-refresh-contents)
+(dolist (package initial-package-list)
+   (package-install package))
 
 
 
