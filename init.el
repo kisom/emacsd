2a484f3dfdae67ac4e657467d202367ed0d0a4ad
Kyle Isom <kyle@cloudflare.com>
2015-02-11 22:46:09 -0800

    Show .emacs load time.

--- init.el
+++ init.el
@@ -1,18 +1,29 @@
 ;;; Load my personal Lisp files.
 
-(setq lisp-base "~/.emacs.d/lisp/")
-(setq lisp-files '("custom.el"    ; first, load customized-*
-                   "personal.el"  ; any high-level customisations
-		   "haskell.el"   ; fun with haskell-mode
-                   "lisps.el"     ; lisp activations for great success
-		   "scpaste.el"   ; scpaste is great for sharing things
-		   "dylan.el"     ; need some dylan
-))
+(let ((startup-time (current-time)))
+  (setq inhibit-startup-echo-area-message
+	"kyle")
+  (setq lisp-base "~/.emacs.d/lisp/")
+  (setq lisp-files '("custom.el"    ; first, load customized-*
+		     "personal.el"  ; any high-level customisations
+		     "haskell.el"   ; fun with haskell-mode
+		     "lisps.el"     ; lisp activations for great success
+		     "scpaste.el"   ; scpaste is great for sharing things
+		     "dylan.el"     ; need some dylan
+		     ))
 
-(mapcar #'load
-	(mapcar (lambda (el)
-		  (concat lisp-base el))
-		lisp-files))
+  (mapcar #'load
+	  (mapcar (lambda (el)
+		    (concat lisp-base el))
+		  lisp-files))
 
-(unless (null window-system)
-  (load-theme 'brin))
+  (unless (null window-system)
+    (load-theme 'brin))
+ 
+  (message (concat
+	    "Start up time: "
+	    (format-time-string "%3N" 
+				(time-subtract
+				 (current-time)
+				 startup-time))
+	    "ms")))
