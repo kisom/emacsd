b0a461b2e12d851435e68f65083b363d6aaf9d33
Kyle <kyle@tyrfingr.is>
2015-03-19 16:10:04 -0700

    add the reverse too

--- lisp/personal.el
+++ lisp/personal.el
@@ -61,8 +61,14 @@
 (global-set-key (kbd "C-c C-;") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-c ;") 'comment-or-uncomment-region)
 
-;; convert markdown links to org links
+;; convert markdown links to org links, but also the other way because
+;; some people just need their markdown
 (defun markdown-link->org ()
   (interactive)
-  (while (search-forward-regexp "\\[\\(.+\\)\\](\\(.+\\))")
+  (while (search-forward-regexp "\\[\\(.+?\\)\\](\\(.+?\\))")
     (replace-match (format "[[%s][%s]]" (match-string 2) (match-string 1)))))
+
+(defun org->markdown-link ()
+  (interactive)
+  (while (search-forward-regexp "\\[\\[\\(.+?\\)\\]\\[\\(.+?\\)\\]\\]")
+    (replace-match (format "[%s](%s)" (match-string 2) (match-string 1)))))
