c7943fe2262175958b6c2e9c635b894488678dc1
Kyle Isom <kyle@cloudflare.com>
2015-03-31 10:41:23 -0700

    Better time functions.
    C-t t is the prefix for the time functions.
    
      C-t t d inserts a date stamp.
      C-t t t inserts a full timestamp.

--- lisp/personal.el
+++ lisp/personal.el
@@ -77,4 +77,10 @@
   (interactive)
   (insert (format-time-string "%Y-%m-%dT%H:%M:%S%z")))
 
-(global-set-key (kbd "C-c T") 'k-insert-timestamp)
+(defun k-insert-date ()
+  (interactive)
+  (insert (format-time-string "%T-%m-%d")))
+
+(global-set-key (kbd "C-c t t") 'k-insert-timestamp)
+(global-set-key (kbd "C-c t d") 'k-insert-date)
+
