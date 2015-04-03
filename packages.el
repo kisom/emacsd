0ef8fea74960dc5cf0d46b30303726866b63a25a
Kyle <kyle@tyrfingr.is>
2015-04-03 15:35:00 -0700

    Update package list.

--- packages.el
+++ packages.el
@@ -5,12 +5,11 @@
 (package-initialize)
 
 ;; get this package list with C-h v package-activated-list
-(setq initial-package-list '(bubbleberry-theme
+(setq initial-package-list '(
+			     bubbleberry-theme
 			     cider
-			     clojure-mode
 			     clojurescript-mode
-			     dash
-			     dylan-mode
+			     dylan-mode epresent
 			     flymake-haskell-multi
 			     flymake-hlint
 			     flymake-easy
@@ -19,20 +18,55 @@
 			     go-mode
 			     haskell-mode
 			     hippie-expand-haskell
+			     kibit-mode
+			     clojure-mode
 			     magit
 			     git-rebase-mode
 			     git-commit-mode
 			     markdown-mode
+			     merlin
+			     mode-compile
+			     niclein
+			     org-ac
+			     auto-complete-pcmp
+			     log4e
+			     auto-complete
+			     org-beautify-theme
+			     org-journal
+			     org-pandoc
+			     org-present
+			     org-presie
+			     eimp
+			     framesize
+			     key-chord
+			     org-projectile
+			     org-wc
+			     orgbox
+			     ox-reveal
+			     org
 			     paredit
+			     popup
+			     projectile
+			     pkg-info
+			     epl
 			     queue
 			     rainbow-delimiters
 			     scpaste
+			     shadchen
+			     slamhound
 			     slime
 			     slime-theme
+			     smartparens
+			     dash
 			     sublime-themes
+			     caml
 			     transpose-frame
 			     tronesque-theme
+			     tuareg
 			     undo-tree
+			     utop
+			     w3m
+			     yaxception
 ))
 
 (add-to-list 'package-archives
