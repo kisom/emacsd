f6c2073a7cec2c47de01b8b1c17bfcff1e96a09f
Kyle Isom <kyle@cloudflare.com>
2015-08-31 23:39:13 -0700

    Cleaning things up.

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
   (setq lisp-files '("custom.el"    ; first, load customized-*
 		     "util.el"      ; load any utilities needed
 		     "personal.el"  ; any high-level customisations
+		     "enabled.el"   ; turn on any default-disabled functions
 		     "haskell.el"   ; fun with haskell-mode
 		     "lisps.el"     ; lisp activations for great success
 		     "scpaste.el"   ; scpaste is great for sharing things
@@ -30,7 +31,7 @@
   ;; load scriba from contrib
   (load "~/.emacs.d/contrib/scriba.el")
 
-  (load-theme 'bubbleberry)
+  (load-theme 'slime)
 
   ;; set eshell path after exec-path is finalised
   (setenv "PATH"
@@ -46,16 +47,3 @@
 			(current-time)
 			startup-time))))))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" "afc220610bee26945b7c750b0cca03775a8b73c27fdca81a586a0a62d45bbce2" "26247bcb0b272ec9a5667a6b854125450c88a44248123a03d9f242fd5c6ec36f" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "869b11b64da20b6b04e9b18721e03a58e5d9f0ee3a7a91bfe7cdc2b24a828109" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
-(put 'downcase-region 'disabled nil)
--- lisp/lisps.el
+++ lisp/lisps.el
@@ -4,13 +4,19 @@
 (require 'paredit)
 
 ;;; Common Lisp
-
 (setq inferior-lisp-program "sbcl")
 (require 'slime)
 (slime-setup '(slime-fancy))    ;adds some nice features
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
-(add-hook 'common-lisp-mode #'enable-paredit-mode)
-(add-hook 'lisp-mode #'enable-paredit-mode)
+
+(require 'eldoc) ; if not already loaded
+;; when moving backwards/forwards a sexp, update eldoc.
+(eldoc-add-command
+ 'paredit-backward-delete
+ 'paredit-close-round)
+
+(add-hook 'lisp-mode-hook             'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 
 ;; asd files need love too
 (add-to-list 'auto-mode-alist
@@ -19,11 +25,8 @@
 			       (paredit-mode))))
 
 ;;; ELisp
-
 (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 
-
 ;;; Clojure
-
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 (add-hook 'clojurescript-mode-hook #'enable-paredit-mode)
