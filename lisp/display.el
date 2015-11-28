68b53fa762179b69377dc907ec0ad2b5d0ef4bf5
Kyle Isom <coder@kyleisom.net>
2015-11-28 15:09:20 -0800

    Try to set font even when not in a window system.
    On most systems, emacs is started as the server process, which doesn't
    pick up that it's being used primarily via the window system. Therefore,
    display.el has to be loaded manually. This change is an attempt at
    fixing this behaviour.

--- lisp/display.el
+++ lisp/display.el
@@ -7,12 +7,12 @@
     (set-frame-parameter nil 'font font)
     (set-face-attribute 'default nil :font font)))
 
-(when window-system
-  (let* ((ffam (font-family-list))
-	 (font (or (font-present-p "Triplicate T4c" ffam)
-		   (font-present-p "Inconsolata" ffam)
-		   (font-present-p "Anonymous Pro" ffam)
-		   (font-present-p "Ubuntu Mono" ffam))))
+(let* ((ffam (font-family-list))
+       (font (or (font-present-p "Triplicate T4c" ffam)
+		 (font-present-p "Inconsolata" ffam)
+		 (font-present-p "Anonymous Pro" ffam)
+		 (font-present-p "Ubuntu Mono" ffam))))
+  (when font
     (pcase (system-name)
       ("tessier" (set-font font 12))
       ("straka" (set-font font 11))
