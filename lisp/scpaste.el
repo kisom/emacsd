7b2884a7d2d11cd713da20bc18d837cb97ac7ab7
kyle@tyrfingr.is <kyle@cloudflare.com>
2016-01-16 13:36:01 -0800

    Add ox-confluence and update scpaste.

--- lisp/scpaste.el
+++ lisp/scpaste.el
@@ -1,3 +1,3 @@
 (require 'scpaste)
 (setq scpaste-http-destination "http://p.kyleisom.net"
-      scpaste-scp-destination "p.kyleisom.net:sites/p/")
+      scpaste-scp-destination "port.tyrfingr.is:sites/p/")
